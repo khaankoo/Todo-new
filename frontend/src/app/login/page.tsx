cca0a6c39c4b21d1e9cd6818b8d04d2b7afb984d
Khangerel Be-ochir <khangeerelbeochir@gmail.com>
2024-01-24 23:04:43 +0800

    ga

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,33 +1,49 @@
 "use client"
 import axios from "axios"
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 
 
 const Login = () => {
+  const router = useRouter();
+  const [ error, setError ] = useState('');
+
+  const [ input, setInput ] = useState({
+    username: '',
+    password: ''
+  });
+
+
   const BASE_URL = "http://localhost:8000/auth/logIn";
 
   const submitHandler = async (e: any) => {
     e.preventDefault()
-    const result = await axios.post(BASE_URL, {
-      username: 'Khangerel',
-      password: '2345'
-    })
-    console.log(result);
-    
+    try {
+        const { data } = await axios.post(BASE_URL, {
+            ...input,
+        })
+        localStorage.setItem('user', JSON.stringify(data.user));
+
+        router.push("/dashboard")
+    } catch (error) {
+        setError(error.response.data.msg)
+    }
   }
   return (
     <div>
-      <h1>Login</h1>
-      <form className="flex flex-col">
-        <label htmlFor="">
-          Username
-          <input type="text" placeholder="Enter your Username" className="border ml-2"/>
-        </label>
-        <label htmlFor="">
-          Password
-          <input type="text" placeholder="Enter your Password" className="border ml-2"/>
-        </label>
-        <button className="w-fit bg-gray-800 text-white p-2" onClick={submitHandler}>Login</button>
-      </form>
+        <h1>Login</h1>
+        <form className="flex flex-col">
+            <label htmlFor="">
+                Username
+                <input type="text" placeholder="Enter your Username" className="border ml-2" onChange={(e) => setInput((prev) => ({ ...prev, username: e.target.value}))}/>
+            </label>
+            <label htmlFor="">
+                Password
+                <input type="text" placeholder="Enter your Password" className="border ml-2" onChange={(e) => setInput((prev) => ({ ...prev, password: e.target.value}))}/>
+            </label>
+            <button className="w-fit bg-gray-800 text-white p-2" onClick={submitHandler}>Login</button>
+        </form>
     </div>
   )
 }
