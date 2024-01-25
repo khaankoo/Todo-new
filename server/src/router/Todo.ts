3eb63d9cdf029aba8e06b6f26ba9425db49e71cc
Khangerel Be-ochir <khangeerelbeochir@gmail.com>
2024-01-25 22:54:52 +0800

    push

--- server/src/router/Todo.ts
+++ server/src/router/Todo.ts
@@ -1,5 +1,10 @@
 import express from "express";
+import { createTodo, getAllTodo, getAllTodoByUserId, getTodo } from "../controller/Todo";
 
 const todo = express.Router();
+
+todo.route('/').post(createTodo);
+todo.route('/').get(getAllTodo);
+todo.route('/id').get(getTodo).get(getAllTodo)
 
 export { todo }
